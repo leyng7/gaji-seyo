c6a1195d6197f11bc41971efabe46c85de39a3b9
UNRYEONG CHEON <unryeong@UNRYEONGui-MacBookPro.local>
2022-09-29 23:47:17 +0900

    first commit

---
+++ src/main/java/com/gajiseyo/GajiSeyoApplication.java
@@ -0,0 +1,13 @@
+package com.gajiseyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GajiSeyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GajiSeyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gajiseyo/GajiSeyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gajiseyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GajiSeyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
