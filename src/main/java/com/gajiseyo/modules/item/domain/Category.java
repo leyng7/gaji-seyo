d883a2e36ae24f606d487d7627ac6466897a586e
sungeunn <saeun0512@gmail.com>
2022-10-04 23:52:24 +0900

    feat: Category/Status enum 추가

--- src/main/java/com/gajiseyo/modules/item/domain/Category.java
+++ src/main/java/com/gajiseyo/modules/item/domain/Category.java
@@ -1,4 +1,33 @@
 package com.gajiseyo.modules.item.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Category {
+
+  DIGITAL_DEVICE("디지털기기"),
+  HOUSEHOLD_APPLIANCES("생활가전"),
+  FURNITURE("가구"),
+  LIVING("생활"),
+  INFANT_CHILD("유아동"),
+  CHILDREN_BOOKS("유아 도서"),
+  WOMEN_CLOTHING("여성 의류"),
+  WOMEN_ACCESSORIES("여성 잡화"),
+  MEN_FASHION_ACCESSORIES("남성 패션잡화"),
+  BEAUTY("미용"),
+  SPORTS("스포츠"),
+  HOBBY_GAME_RECORD("취미 게임 음반"),
+  BOOKS("도서"),
+  USED_CAR("중고차"),
+  TICKET("티켓"),
+  PROCESSED_FOOD("가공식품"),
+  PET_SUPPLIES("반려동물용품"),
+  PLANT("식물"),
+  ETC("기타"),
+  BUY("삽니다");
+
+  private final String displayValue;
+
 }
--- src/main/java/com/gajiseyo/modules/item/domain/Status.java
+++ src/main/java/com/gajiseyo/modules/item/domain/Status.java
@@ -1,4 +1,16 @@
 package com.gajiseyo.modules.item.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Status {
+
+  SALE("판매"),
+  RESERVED("예약"),
+  SOLD_OUT("판매 완료");
+
+  private final String displayValue;
+
 }
