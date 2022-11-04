873bce270e2cca7427b016f4b53cf96550af001d
sungeunn <saeun0512@gmail.com>
2022-11-04 20:05:00 +0900

    feat: 가지생활 list 추가

--- src/main/java/com/gajiseyo/modules/life/controller/LifeController.java
+++ src/main/java/com/gajiseyo/modules/life/controller/LifeController.java
@@ -1,14 +1,22 @@
 package com.gajiseyo.modules.life.controller;
 
+import com.gajiseyo.modules.life.service.LifeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class LifeController {
 
-    public String list() {
-        return null;
+    private final LifeService lifeService;
+
+    @GetMapping("/lives")
+    public String list(Model model) {
+
+        model.addAttribute("list", lifeService.findAll());
+        return "user/life/list";
     }
 
 }
--- src/main/java/com/gajiseyo/modules/life/domain/Life.java
+++ src/main/java/com/gajiseyo/modules/life/domain/Life.java
@@ -1,16 +1,61 @@
 package com.gajiseyo.modules.life.domain;
 
+import com.gajiseyo.modules.common.BaseTimeEntity;
 import com.gajiseyo.modules.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class Life {
+import javax.persistence.*;
 
+@Table(name = "TB_LIFE")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Life extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "life_id")
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private Category category;
+
     private String contents;
+
     private int views;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
+
     private boolean removed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reg_id")
     private Member createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "last_up_id")
     private Member lastUpdateBy;
+
+    private Life(Category category, String contents, Member createdBy) {
+        this.category = category;
+        this.contents = contents;
+        this.views = 0;
+        this.status = Status.LOOK_FOR;
+        this.removed= false;
+        this.createdBy = createdBy;
+        this.lastUpdateBy = createdBy;
+    }
+
+    public static Life create(Category category, String contents, Member createdBy) {
+        return new Life(category, contents, createdBy);
+    }
+
+    public void update(Category category, String contents) {
+        this.category = category;
+        this.contents = contents;
+    }
 
 }
